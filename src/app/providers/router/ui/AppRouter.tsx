8ef02c261b8cfe862b6fd9f4e548db7f1aa795cf
Twisty Skrill <egor.kobzev2018@yandex.ru>
2025-09-03 22:26:49 +0300

    add i18n and base ui kit

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,21 @@
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/AboutPage/MainPage";
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+
+export const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+          <Route
+            key={path}
+            element={<div className="page-wrapper">{element}</div>}
+            path={path}
+          />
+        ))}
+      </Routes>
+    </Suspense>
+  );
+};
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Navbar.module.scss";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink
+          to={"/"}
+          className={cls.mainLink}
+          theme={AppLinkTheme.SECONDARY}
+        >
+          Главная
+        </AppLink>
+        <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
+          О сайте
+        </AppLink>
+      </div>
+    </div>
+  );
+};
