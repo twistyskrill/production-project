a6ba0dd75976d307ac74810dd7c6f0d3c8f7a58f
Twisty Skrill <egor.kobzev2018@yandex.ru>
2025-09-09 12:53:31 +0300

    add visual tools, fix screen

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,14 +2,15 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { Sidebar } from "./Sidebar";
 import { withTranslation } from "react-i18next";
 import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
 
 describe("Sidebar", () => {
   test("test", () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
   test("test sidebar func", () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId("sidebar-toggle");
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
     fireEvent.click(toggleBtn);
