54e7f0f4e636d1f7873a7c28264153a45aa159dc
Twisty Skrill <egor.kobzev2018@yandex.ru>
2025-10-02 14:38:05 +0300

    add articles & articles details page, added it to route config

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,23 +6,24 @@ import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
 import { buildDevServer } from "./buildDevServer";
 export function buildWebpackConfig(
-  options: BuildOptions
+	options: BuildOptions
 ): webpack.Configuration {
-  const { paths, mode, isDev } = options;
-  return {
-    mode,
-    entry: paths.entry,
-    output: {
-      filename: "[name].[contenthash].js",
-      path: paths.build,
-      clean: true,
-    },
-    plugins: buildPlugins(options),
-    module: {
-      rules: buildLoaders(options),
-    },
-    resolve: buildResolvers(options),
-    devtool: isDev ? "inline-source-map" : undefined,
-    devServer: isDev ? buildDevServer(options) : undefined,
-  };
+	const { paths, mode, isDev } = options;
+	return {
+		mode,
+		entry: paths.entry,
+		output: {
+			filename: "[name].[contenthash].js",
+			path: paths.build,
+			clean: true,
+			publicPath: "/",
+		},
+		plugins: buildPlugins(options),
+		module: {
+			rules: buildLoaders(options),
+		},
+		resolve: buildResolvers(options),
+		devtool: isDev ? "inline-source-map" : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined,
+	};
 }
