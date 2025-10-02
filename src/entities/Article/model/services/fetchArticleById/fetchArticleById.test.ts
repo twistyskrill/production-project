097523d11a6b083a08020cd83af1d6b09d5e8073
Twisty Skrill <egor.kobzev2018@yandex.ru>
2025-10-02 19:40:03 +0300

    add unit test for async thunk fetch article by id

---
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.test.ts
@@ -0,0 +1,34 @@
+import { userActions } from "entities/User";
+import { TestAsyncThunk } from "shared/lib/tests/testAsyncThunk/testAsyncThunk";
+import { fetchArticleById } from "./fetchArticleById";
+import { Currency } from "entities/Currency";
+import { Country } from "entities/Country";
+import { Article } from "../../types/article";
+
+const data: Article = {
+	id: "1",
+	title: "js",
+	views: 1765,
+	subtitle: "",
+	img: "",
+	createdAt: "",
+	type: [],
+	blocks: [],
+};
+describe("fetchArticleById", () => {
+	test("success", async () => {
+		const thunk = new TestAsyncThunk(fetchArticleById);
+		thunk.api.get.mockReturnValue(Promise.resolve({ data: data }));
+		const result = await thunk.callThunk("1");
+
+		expect(thunk.api.get).toHaveBeenCalled();
+		expect(result.meta.requestStatus).toBe("fulfilled");
+		expect(result.payload).toEqual(data);
+	});
+	test("error login", async () => {
+		const thunk = new TestAsyncThunk(fetchArticleById);
+		thunk.api.get.mockReturnValue(Promise.resolve({ status: 403 }));
+		const result = await thunk.callThunk("1");
+		expect(result.meta.requestStatus).toBe("rejected");
+	});
+});
