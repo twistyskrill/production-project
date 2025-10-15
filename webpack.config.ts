e7ff7746388456bba4e2a89cc953a37117bd99a1
Twisty Skrill <egor.kobzev2018@yandex.ru>
2025-10-16 00:04:39 +0300

    add vercel

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,24 +4,26 @@ import { BuildEnv, BuildPaths } from "./config/build/types/config";
 import path from "path";
 
 export default (env: BuildEnv) => {
-  const paths: BuildPaths = {
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    build: path.resolve(__dirname, "build"),
-    html: path.resolve(__dirname, "public", "index.html"),
-    src: path.resolve(__dirname, "src"),
-  };
+	const paths: BuildPaths = {
+		entry: path.resolve(__dirname, "src", "index.tsx"),
+		build: path.resolve(__dirname, "build"),
+		html: path.resolve(__dirname, "public", "index.html"),
+		src: path.resolve(__dirname, "src"),
+		locales: path.resolve(__dirname, "public", "locales"),
+		buildLocales: path.resolve(__dirname, "build", "locales"),
+	};
 
-  const mode = env.mode || "development";
-  const isDev = mode === "development";
-  const apiUrl = env.apiUrl || "http://localhost:8000";
-  const PORT = env.port || 3000;
-  const config: webpack.Configuration = buildWebpackConfig({
-    mode,
-    paths,
-    isDev,
-    port: PORT,
-    apiUrl,
-    project: "frontend",
-  });
-  return config;
+	const mode = env.mode || "development";
+	const isDev = mode === "development";
+	const apiUrl = env.apiUrl || "http://localhost:8000";
+	const PORT = env.port || 3000;
+	const config: webpack.Configuration = buildWebpackConfig({
+		mode,
+		paths,
+		isDev,
+		port: PORT,
+		apiUrl,
+		project: "frontend",
+	});
+	return config;
 };
